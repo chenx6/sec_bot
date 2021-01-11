b2fa2e1016dd8f8a1105f6883313cd1a4f7a7388
chen_null <abc82766@gmail.com>
2021-01-11 18:31:51 +0800

    添加 LimitCounter

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from aiocqhttp import CQHttp, Event
 from schedule import every
 
 from config import subscribe_groups
-from tools import run_continuously, call_async_func
+from tools import run_continuously, call_async_func, LimitCounter
 from plugin import (base_bot, anquanke_vuln, ctfhub, daily_push, help_menu,
                     whoami, rss, admin, unknown_message)
 from spider.rsshub_weibo import get_xuanwu_push
@@ -24,6 +24,7 @@ plugins: List[base_bot.BaseBot] = [
 send_silent = False  # 机器人禁言
 bot = CQHttp()
 logger = bot.logger
+counter = LimitCounter()
 
 
 @bot.on_message('group')
@@ -31,10 +32,14 @@ async def reply_at(event: Event):
     """
     反馈 at 消息
     """
+    if not counter.can_send():
+        await bot.send(event, f'发送的太快了吧，{event.sender["nickname"]}，让我缓缓(＞﹏＜)')
+        return
     for plugin in plugins:
         if plugin.match(event, event.message):
             reply_text = await plugin.reply(event)
             await bot.send(event, reply_text)
+            counter.add_counter()
             break
 
 
@@ -62,7 +67,12 @@ def send_weekly_push():
         bot.sync.send_group_msg(group_id=gid, message=competitions)
 
 
+def reset_counter():
+    counter.reset_counter()
+
+
 every().day.at('18:00').do(send_daily_push)
 every().friday.at('21:00').do(send_weekly_push)
+every().minutes.do(reset_counter)
 run_continuously(1800)
 bot.run(host='127.0.0.1', port=8080)
--- tools.py
+++ tools.py
@@ -4,6 +4,25 @@ from schedule import run_pending
 from asyncio import new_event_loop, set_event_loop, get_event_loop
 
 
+class LimitCounter:
+    """
+    发送消息频率限制器
+    """
+    def __init__(self, limit=5) -> None:
+        super().__init__()
+        self.count = 0
+        self.limit = limit
+
+    def can_send(self) -> bool:
+        return self.count < self.limit
+
+    def add_counter(self) -> None:
+        self.count += 1
+
+    def reset_counter(self) -> None:
+        self.count = 0
+
+
 def run_continuously(interval=1):
     """Continuously run, while executing pending jobs at each elapsed
     time interval.
