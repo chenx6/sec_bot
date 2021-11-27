5ad3427ed62f6dd891bb03afc5b2ebf93ccbb625
chen_null <abc82766@gmail.com>
2021-11-27 18:02:47 +0800

    添加按照时间段进行查询比赛

--- spider/ctfhub.py
+++ spider/ctfhub.py
@@ -1,12 +1,17 @@
+from typing import Optional
 from aiohttp import ClientSession
 from json import dumps
-from time import strftime, localtime
+from time import strftime, localtime, time
 
 
 CTFHUB_DOMAIN = 'https://api.ctfhub.com'
 
 
 def to_strtime(x: int) -> str: return strftime('%m月%d日 %H:%M:%S', (localtime(x)))
+
+
+def comp_to_str(i) -> str:
+    return f'{i["title"]}\n开始时间：{to_strtime(i["start_time"])}\n结束时间：{to_strtime(i["end_time"])}\n'
 
 
 async def ctfhub_get_upcoming_event(limit=5, offset=0) -> str:
@@ -20,9 +25,28 @@ async def ctfhub_get_upcoming_event(limit=5, offset=0) -> str:
         json = await response.json()
         if not json['status']:
             return ''
-        competitions = []
-        for i in json['data']['items']:
-            start_time = to_strtime(i['start_time'])
-            end_time = to_strtime(i['end_time'])
-            competitions.append(f'{i["title"]}\n开始时间：{start_time}\n结束时间：{end_time}\n')
+        competitions = map(comp_to_str, json['data']['items'])
         return '\n'.join(competitions).strip()
+
+
+async def ctfhub_get_event_by_time(start: Optional[int] = None,
+                                   end: Optional[int] = None):
+    """
+    通过时间进行查询赛事
+    """
+    if not start:
+        start = int(time())
+    if not end:
+        end = start + 604800  # 往后一周
+    async with ClientSession() as session:
+        data = {"start": start, "end": end}
+        response = await session.post(
+            f"{CTFHUB_DOMAIN}/User_API/Event/getCalendar", json=data)
+        json = await response.json()
+        if not json['status']:
+            return ''
+        it = filter(
+            lambda x: x['start_time'] >= start and x['start_time'] < end,
+            json['data']['items'])
+        it = map(comp_to_str, it)
+        return '\n'.join(it).strip()
--- test/ctfhub.py
+++ test/ctfhub.py
@@ -1,12 +1,19 @@
 from unittest import TestCase, main
 from asyncio import get_event_loop
 
-from spider.ctfhub import ctfhub_get_upcoming_event
+from spider.ctfhub import ctfhub_get_upcoming_event, ctfhub_get_event_by_time
 
 class CTFHUBSpiderTest(TestCase):
     def test_upcoming_event(self):
         loop = get_event_loop()
         task = loop.create_task(ctfhub_get_upcoming_event())
+        loop.run_until_complete(task)
+        print(task.result())
+        self.assertTrue(len(task.result()) != 0)
+    
+    def test_ctfhub_time(self):
+        loop = get_event_loop()
+        task = loop.create_task(ctfhub_get_event_by_time())
         loop.run_until_complete(task)
         print(task.result())
         self.assertTrue(len(task.result()) != 0)
